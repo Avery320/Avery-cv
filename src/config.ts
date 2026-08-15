eec89c7430b20703817e889b524a3cb9c637fbf7
AveryTsai <n76124052@gs.ncku.edu.tw>
2026-08-15 23:20:59 +0800

    chore: personalize repository and remove unused assets

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
+export const SITE_TITLE = 'Avery Tsai | CV and Portfolio';
 export const SITE_DESCRIPTION = 'A personal CV, projects, and robotics portfolio built with Astro and TailwindCSS.';
 export const TRANSITION_API = true
