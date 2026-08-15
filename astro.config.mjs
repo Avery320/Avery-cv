27bfd623d838c72a470c57d0636ec486a1190657
AveryTsai <n76124052@gs.ncku.edu.tw>
2026-08-15 22:49:20 +0800

    ci: deploy portfolio to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://avery320.github.io',
+  base: '/Avery-cv',
   integrations: [mdx(), tailwind()]
 });
